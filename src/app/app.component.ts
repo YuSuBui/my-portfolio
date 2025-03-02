5e352839eba4e2662bce204268d5ea2be73a25b6
yusubui <buicongsum2001@gmail.com>
2025-03-02 18:23:52 +0700

    fix: layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-portfolio';
+  searchValues: string = "";
+  list = [
+    {
+      id: "e6c9be40-189b-4c49-9b53-8136319452cc",
+      name: "Intersection",
+      href: "intersection",
+      active: true
+    },
+    {
+      id: "8d6647c8-a16c-4fc4-94d9-cff09214e33c",
+      name: "Item 1",
+      href: "item-a",
+      active: false
+    },
+    {
+      id: "4dd92f46-5dd5-40aa-92b0-ea8564406e0e",
+      name: "Item 3",
+      href: "",
+      active: false
+    }
+  ];
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  get filteredList() {
+    if (this.searchValues.length === 0) {
+      return this.list;
+    }
+
+    return this.list.filter(l => l.name.toLowerCase().includes(this.searchValues.toLowerCase()));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    InputTextModule, //PrimeNG
+    ButtonModule, //PrimeNG
   ],
   providers: [],
   bootstrap: [AppComponent]
